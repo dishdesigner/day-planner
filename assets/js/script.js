bb9d380c3be0bdf3cd64a3368760118d16c9aa39
Darrin Scott Hunter <dish@dishdesign.com>
2021-02-27 15:40:46 -0500

    create background colors based on current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,20 @@
 let currentDayTime = setInterval(function(){
     let currentDay = document.querySelector('#currentDay');
     let currentTime = document.querySelector('#currentTime');
-    let event = document.querySelector('textarea');
+    let appointments = document.querySelectorAll('textarea');
 
     currentDay.innerHTML = moment().format('dddd MMMM Do YYYY');
     currentTime.innerHTML = moment().format('h:mm:ss');
 
-
+    appointments.forEach(appointment => {
+        // console.log(appointment.dataset.hour);
+        // console.log(moment('HH'));
+        if (appointment.dataset.hour < moment().hour()) {
+            appointment.className = 'past';
+        } else if (appointment.dataset.hour == moment().hour()) {
+            appointment.className = 'present';
+        } else {
+            appointment.className = 'future';
+        }
+    })
 }, 1000);
-
