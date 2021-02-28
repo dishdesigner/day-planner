4ec92fbae593ebb0cafb4bdae3f64d2ecc9f23aa
Darrin Scott Hunter <dish@dishdesign.com>
2021-02-27 18:54:10 -1200

    add listeners, time zone testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
+const currentDay = document.querySelector('#currentDay');
+const currentTime = document.querySelector('#currentTime');
+const appointments = document.querySelectorAll('textarea');
+const saveButtons = document.querySelectorAll('div.save');
 
-// Display current day/time, updated each second
+/******************************************
+Display current day/time, updated each second, and update grid background colors based on current time
+*******************************************/
 let currentDayTime = setInterval(function(){
-    let currentDay = document.querySelector('#currentDay');
-    let currentTime = document.querySelector('#currentTime');
-    let appointments = document.querySelectorAll('textarea');
-
     currentDay.innerHTML = moment().format('dddd MMMM Do YYYY');
     currentTime.innerHTML = moment().format('h:mm:ss');
-
+    // update grid colors based on current time
     appointments.forEach(appointment => {
-        // console.log(appointment.dataset.hour);
-        // console.log(moment('HH'));
         if (appointment.dataset.hour < moment().hour()) {
             appointment.className = 'past';
         } else if (appointment.dataset.hour == moment().hour()) {
@@ -20,3 +20,23 @@ let currentDayTime = setInterval(function(){
         }
     })
 }, 1000);
+
+/******************************************
+Write appointments into local storage when Save is clicked and change state of button when saved data exists
+*******************************************/
+function saveAppointment(event) {
+    const appointment = appointments.find(appt => appt.dataset.hour === event.target.dataset.hour);
+    appointment.JSON.stringify
+
+}
+
+saveButtons.addEventListener('click', saveAppointment);
+
+/******************************************
+On change of textarea input, remove .stored class state of Save button on that row to make it active again to invite Saving to local storage.
+*******************************************/
+
+/******************************************
+On page load, check if UserStorage holds values for appointments. If so, populate the textareas and change state of Save buttons.
+*******************************************/
+
