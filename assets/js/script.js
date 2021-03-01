e67ba33ccd517119f9b604863a91940ab026809d
Darrin Scott Hunter <dish@dishdesign.com>
2021-03-01 14:05:26 +0900

    fix appointment grid local storage issues and button states, add removing local store when input is blank

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,35 @@
 const currentDay = document.querySelector('#currentDay');
 const currentTime = document.querySelector('#currentTime');
-const appointments = document.querySelectorAll('textarea');
+const appointmentInputs = document.querySelectorAll('textarea');
 const saveButtons = document.querySelectorAll('div.save');
+
+/******************************************
+Write appointments into local storage when Save is clicked and change state of button
+*******************************************/
+document.addEventListener('click', function (event){
+    let hour2save = event.target.dataset.hour;
+    let appt2save = $('div.row[data-hour = "'+hour2save+'"] * textarea').val(); // OMG, this took a DAY to figure out!
+
+    if (event.target.classList.contains('save')) {
+        console.log(`Hour = ${hour2save}, Appt = ${appt2save}`); // TESTING
+        if (appt2save === "") {
+            localStorage.removeItem(hour2save); // delete any empties
+            event.target.classList.remove('stored');
+        } else {
+            localStorage.setItem(hour2save, appt2save);
+            event.target.classList.add('stored');
+        }
+    }
+}, false); // false to 'capturing' on way down DOM before bubbling back up
 
 /******************************************
 Display current day/time, updated each second, and update grid background colors based on current time
 *******************************************/
-let currentDayTime = setInterval(function(){
+let currentDayTimer = setInterval(function(){
     currentDay.innerHTML = moment().format('dddd MMMM Do YYYY');
     currentTime.innerHTML = moment().format('h:mm:ss');
     // update grid colors based on current time
-    appointments.forEach(appointment => {
+    appointmentInputs.forEach(appointment => {
         if (appointment.dataset.hour < moment().hour()) {
             appointment.className = 'past';
         } else if (appointment.dataset.hour == moment().hour()) {
@@ -20,17 +39,6 @@ let currentDayTime = setInterval(function(){
         }
     })
 }, 1000);
-
-/******************************************
-Write appointments into local storage when Save is clicked and change state of button when saved data exists
-*******************************************/
-function saveAppointment(event) {
-    const appointment = appointments.find(appt => appt.dataset.hour === event.target.dataset.hour);
-    appointment.JSON.stringify
-
-}
-
-saveButtons.addEventListener('click', saveAppointment);
 
 /******************************************
 On change of textarea input, remove .stored class state of Save button on that row to make it active again to invite Saving to local storage.
