463b8d45c33b8444dc3a149347868509f7d132fd
Darrin Scott Hunter <dish@dishdesign.com>
2021-03-01 12:32:53 +0500

    fix maintaining state for appointments after page reload

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,25 +4,6 @@ const appointmentInputs = document.querySelectorAll('textarea');
 const saveButtons = document.querySelectorAll('div.save');
 
 /******************************************
-Write appointments into local storage when Save is clicked and change state of button
-*******************************************/
-document.addEventListener('click', function (event){
-    let hour2save = event.target.dataset.hour;
-    let appt2save = $('div.row[data-hour = "'+hour2save+'"] * textarea').val(); // OMG, this took a DAY to figure out!
-
-    if (event.target.classList.contains('save')) {
-        console.log(`Hour = ${hour2save}, Appt = ${appt2save}`); // TESTING
-        if (appt2save === "") {
-            localStorage.removeItem(hour2save); // delete any empties
-            event.target.classList.remove('stored');
-        } else {
-            localStorage.setItem(hour2save, appt2save);
-            event.target.classList.add('stored');
-        }
-    }
-}, false); // false to 'capturing' on way down DOM before bubbling back up
-
-/******************************************
 Display current day/time, updated each second, and update grid background colors based on current time
 *******************************************/
 let currentDayTimer = setInterval(function(){
@@ -41,10 +22,40 @@ let currentDayTimer = setInterval(function(){
 }, 1000);
 
 /******************************************
-On change of textarea input, remove .stored class state of Save button on that row to make it active again to invite Saving to local storage.
+Write appointments into local storage when Save is clicked and change state of button
 *******************************************/
+document.addEventListener('click', (event) => {
+    let hour2save = event.target.dataset.hour;
+    let appt2save = $('textarea[data-hour = "'+hour2save+'"]').val(); // OMG, this took a DAY to figure out!
+
+    if (event.target.classList.contains('save')) {
+        console.log(`Hour = ${hour2save}, Appt = ${appt2save}`); // TESTING
+        if (appt2save === "") {
+            localStorage.removeItem(hour2save); // delete any empties
+            event.target.classList.remove('stored');
+        } else {
+            localStorage.setItem(hour2save, appt2save);
+            event.target.classList.add('stored');
+        }
+    }
+}, false); // false to 'capturing' on way down DOM before bubbling back up
 
 /******************************************
 On page load, check if UserStorage holds values for appointments. If so, populate the textareas and change state of Save buttons.
 *******************************************/
+window.addEventListener('load', (event) => {
+    appointmentInputs.forEach(appointment => {
+        let hour2read = appointment.dataset.hour;
+        let storedAppt = localStorage.getItem(hour2read);
 
+        // let storedApptButton = $('div.save[data-hour = "'+hour2read+'"]');
+        let storedApptButton = $('.save[data-hour = "'+hour2read+'"]');
+
+        console.log(storedApptButton.classList);
+
+        if (storedAppt !== null) { // if storage exists, restore
+            appointment.value = storedAppt;
+            // storedApptButton.classList.add('stored');
+        }
+    })
+})
